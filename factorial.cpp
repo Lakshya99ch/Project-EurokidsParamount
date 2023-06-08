967ba67ba34189448b477c776cf049a7534e7755
Lakshya Chaudhary <lakshyachaudhary0098@gmail.com>
2023-06-08 22:56:43 +0530

    First Commit

---
+++ factorial.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+
+    int fact = 1;
+    cout<<"Enter the positive integer: ";
+    cin>>n;
+
+    if(n>0){
+        cout<<"Error";
+    }
+    else{
+        for(int i=1; i<=n; i++){
+            fact *=i;
+        }
+        cout<<"factorial of"<<n<< " = "<<fact'
+    }
+    return 0;
+}
